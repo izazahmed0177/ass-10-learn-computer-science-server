db398a4d72b4583e4d56be3819191cd6f71c662a
unknown <izazahmed0177@gmail.com>
2022-10-25 23:34:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+
+const courses = require('./data/courses.json');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
