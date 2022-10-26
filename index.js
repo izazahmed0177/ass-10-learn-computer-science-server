55e99389aab0b41894c2623e293f88562a0db3bb
unknown <izazahmed0177@gmail.com>
2022-10-27 01:55:21 +0600

    checkout route make

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.get('/course/:id', (req, res) => {
     res.send(selectCourse);
 });
 
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+
+    const checkoutID = coursesDetail.find(course => course.course_id === id);
+    res.send(checkoutID);
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
